222ecd84781a248685ea05c4879fd5a36d310ca2
ksgin <imqqyangfan@gmail.com>
2017-07-13 09:12:19 +0800

    complete Substring with Concatenation of All Words

--- LeetCode/SubstringWithConcatenationOfAllWords.cpp
+++ LeetCode/SubstringWithConcatenationOfAllWords.cpp
@@ -4,12 +4,29 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class SubstringWithConcatenationOfAllWords {
 public:
     vector<int> findSubstring(string s, vector<string> &words) {
-
+        vector<int> res;
+        if (s.empty() || words.empty()) return res;
+        int n = words.size(), m = words[0].size();
+        unordered_map<string, int> m1;
+        for (auto &a : words) ++m1[a];
+        for (int i = 0; i <= (int) s.size() - n * m; ++i) {
+            unordered_map<string, int> m2;
+            int j = 0;
+            for (j = 0; j < n; ++j) {
+                string t = s.substr(i + j * m, m);
+                if (m1.find(t) == m1.end()) break;
+                ++m2[t];
+                if (m2[t] > m1[t]) break;
+            }
+            if (j == n) res.push_back(i);
+        }
+        return res;
     }
 };
