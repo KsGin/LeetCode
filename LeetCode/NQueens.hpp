75405fe5487029f18f4665ce9b428bf42b8ca224
ksgin <imqqyangfan@gmail.com>
2017-08-22 09:32:47 +0800

    Start NQ

---
+++ LeetCode/NQueens.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class NQueens {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ret(0);
+
+        return ret;
+    }
+};
