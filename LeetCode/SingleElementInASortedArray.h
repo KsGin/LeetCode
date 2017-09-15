d65542e9b8769b4ad6456dd5890474e09616099c
ksgin <imqqyangfan@gmail.com>
2017-09-15 21:21:24 +0800

    Start Single Element in a Sorted Array

---
+++ LeetCode/SingleElementInASortedArray.h
@@ -0,0 +1,19 @@
+/*
+ * File Name : SingleElementInASortedArray
+ * Author    : KsGin
+ * Date      : 9/15/2017
+ */
+
+#ifndef LEETCODE_SINGLEELEMENTINASORTEDARRAY_H
+#define LEETCODE_SINGLEELEMENTINASORTEDARRAY_H
+
+#include <vector>
+
+class SingleElementInASortedArray {
+public:
+    int singleNonDuplicate(std::vector<int>& nums) {
+        
+    }
+};
+
+#endif //LEETCODE_SINGLEELEMENTINASORTEDARRAY_H
