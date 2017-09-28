50ead4ccadda1de8c8e8d7e9f8c6c45298c0dde4
杨帆 <imqqyangfan@gmail.com>
2017-09-28 13:09:43 +0800

    update BaseballGame

--- LeetCode/BaseballGame.h
+++ LeetCode/BaseballGame.h
@@ -9,10 +9,15 @@
 
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <cstdio>
 
 class BaseballGame {
 public:
     int calPoints(std::vector<std::string>& ops) {
+        if (ops.size() == 0) return 0;
+
+        int lf = atoi(ops[0].c_str());
 
     }
 };
