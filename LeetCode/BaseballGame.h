5d8360e5ab0c77c33cdc490d56d5ce2e34c23a37
ksgin <imqqyangfan@gmail.com>
2017-09-28 10:05:03 +0800

    Update BaseballGame

---
+++ LeetCode/BaseballGame.h
@@ -0,0 +1,20 @@
+/*
+ * File Name : BaseballGame
+ * Author    : KsGin
+ * Date      : 9/26/2017
+ */
+
+#ifndef LEETCODE_BASEBALLGAME_H
+#define LEETCODE_BASEBALLGAME_H
+
+#include <string>
+#include <vector>
+
+class BaseballGame {
+public:
+    int calPoints(std::vector<std::string>& ops) {
+
+    }
+};
+
+#endif //LEETCODE_BASEBALLGAME_H
