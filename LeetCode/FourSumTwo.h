3f2723e4c6f406ed5acf772a4626a6540afa33f5
KsGin <imqqyangfan@gmail.com>
2017-11-12 11:24:55 +0800

    update 4Sum II

--- LeetCode/FourSumTwo.h
+++ LeetCode/FourSumTwo.h
@@ -11,35 +11,7 @@
 #include <unordered_map>
 
 class FourSumTwo {
-
-    void shit(std::vector<std::vector<int>> vcs , int dep , int tar , int& cot){
-        if(dep >= vcs.size())
-            return;
-
-        if(dep == vcs.size()-1){
-            for (int i = 0; i < vcs[dep].size(); ++i) {
-                if(tar - vcs[dep][i] == 0){
-                    ++cot;
-                }
-            }
-            return;
-        }
-
-        for (int i = 0; i < vcs[dep].size(); ++i) {
-            shit(vcs , dep+1 , tar - vcs[dep][i] , cot);
-        }
-    }
-
 public:
-    /*O(N^4) Time Limit Exceeded*/
-    int fourSumCountW(std::vector<int>& A, std::vector<int>& B, std::vector<int>& C, std::vector<int>& D) {
-        std::vector<std::vector<int>> fuck { A , B , C , D};
-        int cot = 0;
-        shit(fuck , 0 , 0 , cot);
-        return cot;
-    }
-
-    /*AC*/
     int fourSumCount(std::vector<int>& A, std::vector<int>& B, std::vector<int>& C, std::vector<int>& D) {
         int res = 0;
         std::unordered_map<int, int> m;
